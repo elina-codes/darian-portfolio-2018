60cb915f473dc29c67ddb0ec25173789bfa7a96c
Elina <elinagoldin@gmail.com>
2019-06-28 23:05:32 -0700

    Using absolute links

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,26 @@ const mobileNavOverlay = document.getElementById("js--mobile-nav-overlay");
 const activeClass = "active";
 
 const toggleMobileNav = () => {
-    document.body.classList.toggle("open-nav");
+  document.body.classList.toggle("open-nav");
 };
 
 const closeMobileNav = () => {
-    document.body.classList.remove("open-nav");
+  document.body.classList.remove("open-nav");
 };
 
 mobileNavToggle.onclick = toggleMobileNav;
 mobileNavOverlay.onclick = closeMobileNav;
 
 mobileNav.addEventListener("click", function(e) {
-    if (e.target && (e.target.nodeName == "SPAN" || e.target.nodeName == "A")) {
-        closeMobileNav();
-        document.querySelector("#js--mobile-nav a.active").classList.remove(activeClass);
-        e.target.parentElement.classList.add(activeClass);
-    }
+  if (e.target && (e.target.nodeName == "SPAN" || e.target.nodeName == "A")) {
+    closeMobileNav();
+    document
+      .querySelector("#js--mobile-nav a.active")
+      .classList.remove(activeClass);
+    e.target.parentElement.classList.add(activeClass);
+  }
 });
+
+if (!window.location.hostname.includes("uxitall")) {
+  document.querySelector(".nav-secondary").classList.remove("hide");
+}
